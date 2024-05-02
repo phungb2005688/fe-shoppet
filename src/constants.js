9507587c9263d48320c5dee8a8d3f6845a6c31ab
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2024-05-02 15:05:15 +0700

    connect backend

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
-export const BASE_URL = ''; // If using proxy
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL || '';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
